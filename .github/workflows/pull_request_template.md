9021620613c22211fbc7ab1a5deb1332616d112e
Mwandamena <mwandamenakelvin777@gmail.com>
2023-12-19 16:37:30 -0800

    Added components folder, testing files & assets

---
+++ .github/workflows/pull_request_template.md
@@ -0,0 +1,16 @@
+### What?
+
+
+### Why?
+
+
+### How?
+
+
+### Testing?
+
+
+### Screenshots (optional)
+
+
+### Anything else? (optional)
---
+++ frontend/src/components/index.js
@@ -0,0 +1,50 @@
+// IMPORT ALL COMPONENTS AND EXPORT THEM IN A SINGLE FILE
+
+
+// common components
+import Button from "./common/Button";
+import Container from "./common/Container";
+
+// navbar components
+import Navbar from "./navbar/Navbar";
+import Navlinks from "./navbar/Navlinks";
+import CallToAction from "./navbar/CallToAction";
+import Logo from "./navbar/Logo";
+
+// hero components
+import Hero from "./hero/Hero";
+import HeroImage from "./hero/HeroImage";
+import HeroText from "./hero/HeroText";
+
+// grid components
+import Grid from "./grid/Grid";
+import Card from "./grid/card/Card";
+import Filter from "./grid/search/Filter";
+import Pagination from "./grid/search/Pagination";
+import Search from "./grid/search/Search";
+
+
+// footer components
+import Footer from "./footer/Footer";
+
+
+
+
+// EXPORT ALL COMPONENTS
+export {
+    Button,
+    Container,
+    Navbar,
+    Navlinks,
+    CallToAction,
+    Logo,
+    Hero,
+    HeroImage,
+    HeroText,
+    Grid,
+    Search,
+    Filter,
+    Pagination,
+    Card,
+    Footer,
+}
